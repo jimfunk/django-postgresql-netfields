9f4919aab985fad12ec541a146266579a78930d2
James Oakley <jfunk@funktronics.ca>
2019-09-10 06:52:35 -0700

    1.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.version_info.major == 2:
 
 setup(
     name='django-netfields',
-    version='1.0.1',
+    version='1.1.0',
     license='BSD',
     description='Django PostgreSQL netfields implementation',
     long_description=get_long_description(),
