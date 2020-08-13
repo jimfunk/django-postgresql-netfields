241f61df16f46e105c04616b7a4d2010a0c118f5
Gal Shem-Tov <gal.s@curv.co>
2020-08-13 18:01:16 +0300

    Change explicit table to use meta.

--- test/tests/test_sql_fields.py
+++ test/tests/test_sql_fields.py
@@ -489,7 +489,7 @@ class TestIPv4InetField(BaseInetFieldTestCase, TestCase):
     def setUp(self):
         self.model = IPv4InetTestModel
         self.qs = self.model.objects.all()
-        self.table = 'ipv4inet'
+        self.table = self.model.Meta.db_table
 
     def test_save_ipv4_address_type(self):
         instance = self.model.objects.create(field='10.1.2.3/24')
@@ -508,7 +508,7 @@ class TestIPv6InetField(BaseInetFieldTestCase, TestCase):
     def setUp(self):
         self.model = IPv6InetTestModel
         self.qs = self.model.objects.all()
-        self.table = 'ipv6inet'
+        self.table = self.model.Meta.db_table
 
     def test_save_ipv4_address_type_fails(self):
         self.assertRaises(ValidationError, self.model(field='10.1.2.3/24').save)
@@ -583,7 +583,7 @@ class TestIPv4CidrField(BaseCidrFieldTestCase, TestCase):
     def setUp(self):
         self.model = IPv4CidrTestModel
         self.qs = self.model.objects.all()
-        self.table = 'ipv4cidr'
+        self.table = self.model.Meta.db_table
 
     def test_save_ipv4_address_type(self):
         instance = self.model.objects.create(field='10.1.2.0/24')
@@ -602,7 +602,7 @@ class TestIPv6CidrField(BaseCidrFieldTestCase, TestCase):
     def setUp(self):
         self.model = IPv6CidrTestModel
         self.qs = self.model.objects.all()
-        self.table = 'ipv6cidr'
+        self.table = self.model.Meta.db_table
 
     def test_save_ipv4_address_type_fails(self):
         self.assertRaises(ValidationError, self.model(field='10.1.2.0/24').save)
