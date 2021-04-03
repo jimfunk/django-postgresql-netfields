2daf49da8627c2b70bf403e57e77ead44f0c4f26
James Oakley <jfunk@pete.funktronics.ca>
2021-04-03 14:14:27 -0300

    Avoid deprecation warning on Django 2.x. Fixes #111

--- netfields/compat.py
+++ netfields/compat.py
@@ -1,8 +1,11 @@
 from django import VERSION
 
-if VERSION[0] <= 2:
+if VERSION[0] < 2:
     from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper
-    from django.utils.six import with_metaclass, text_type
 else:
     from django.db.backends.postgresql.base import DatabaseWrapper
+
+if VERSION[0] <= 2:
+    from django.utils.six import with_metaclass, text_type
+else:
     from six import with_metaclass, text_type
